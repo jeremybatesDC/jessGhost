99f57051bbd6e9bd858dcd2f945c0e5a67727d4f
jB <jeremy.bates@gmail.com>
2015-03-13 20:33:21 -0400

    initial commit

---
+++ stiky_1.0.0_2/stiky/assets/js/index.js
@@ -0,0 +1,187 @@
+/**
+ * Main JS file for the theme behaviours
+ */
+
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+
+
+/* --------------------------------------------------------- */
+// Expandable Navigation
+/* --------------------------------------------------------- */
+
+jQuery(document).ready(function($){
+  var $lateral_menu_trigger = $('#cd-menu-trigger'),
+    $content_wrapper = $('.cd-main-content'),
+    $navigation = $('header.pxn');
+
+  //open-close lateral menu clicking on the menu icon
+  $lateral_menu_trigger.on('click', function(event){
+    event.preventDefault();
+    
+    $lateral_menu_trigger.toggleClass('is-clicked');
+    $navigation.toggleClass('lateral-menu-is-open');
+    $content_wrapper.toggleClass('lateral-menu-is-open').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+      // firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
+      $('body').toggleClass('overflow-hidden');
+    });
+    $('#cd-lateral-nav').toggleClass('lateral-menu-is-open');
+    
+    //check if transitions are not supported - i.e. in IE9
+    if($('html').hasClass('no-csstransitions')) {
+      $('body').toggleClass('overflow-hidden');
+    }
+  });
+
+  //close lateral menu clicking outside the menu itself
+  $content_wrapper.on('click', function(event){
+    if( !$(event.target).is('#cd-menu-trigger, #cd-menu-trigger span') ) {
+      $lateral_menu_trigger.removeClass('is-clicked');
+      $navigation.removeClass('lateral-menu-is-open');
+      $content_wrapper.removeClass('lateral-menu-is-open').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+        $('body').removeClass('overflow-hidden');
+      });
+      $('#cd-lateral-nav').removeClass('lateral-menu-is-open');
+      //check if transitions are not supported
+      if($('html').hasClass('no-csstransitions')) {
+        $('body').removeClass('overflow-hidden');
+      }
+
+    }
+  });
+
+  //open (or close) submenu items in the lateral menu. Close all the other open submenu items.
+  $('.item-has-children').children('a').on('click', function(event){
+    event.preventDefault();
+    $(this).toggleClass('submenu-open').next('.sub-menu').slideToggle(200).end().parent('.item-has-children').siblings('.item-has-children').children('a').removeClass('submenu-open').next('.sub-menu').slideUp(200);
+  });
+
+});
+
+      /* --------------------------------------------------------- */
+      // Header scrollToFixed
+      /* --------------------------------------------------------- */
+
+      // $('header').scrollToFixed();
+
+      /* --------------------------------------------------------- */
+      // fitVids
+      /* --------------------------------------------------------- */
+
+        $(".post-content, .post-excerpt").fitVids();
+
+        function pxnFullImg() {
+            $("img").each( function() {
+                var contentWidth = $(".post-content, .post-excerpt").outerWidth(); // Width of the content
+                var imageWidth = $(this)[0].naturalWidth; // Original image resolution
+
+                if (imageWidth >= contentWidth) {
+                    $(this).addClass('full-img');
+                } else {
+                    $(this).removeClass('full-img');
+                }
+            });
+        };
+
+        pxnFullImg();
+        $(window).smartresize(pxnFullImg);
+
+        $(".scroll-down").arctic_scroll();
+
+    });
+
+}(jQuery));
+
+(function($,sr){
+
+  // debouncing function from John Hann
+  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+  var debounce = function (func, threshold, execAsap) {
+      var timeout;
+
+      return function debounced () {
+          var obj = this, args = arguments;
+          function delayed () {
+              if (!execAsap)
+                  func.apply(obj, args);
+              timeout = null;
+          };
+
+          if (timeout)
+              clearTimeout(timeout);
+          else if (execAsap)
+              func.apply(obj, args);
+
+          timeout = setTimeout(delayed, threshold || 100);
+      };
+  }
+  // smartresize 
+  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+})(jQuery,'smartresize');
+
+// Arctic Scroll by Paul Adam Davis
+// https://github.com/PaulAdamDavis/Arctic-Scroll
+(function ($) {
+    $.fn.arctic_scroll = function (options) {
+
+        var defaults = {
+            elem: $(this),
+            speed: 500
+        };
+        var options = $.extend(defaults, options);
+
+        options.elem.click(function(event){     
+            event.preventDefault();
+            var offset = ($(this).attr('data-offset')) ? $(this).attr('data-offset') : false,
+                position = ($(this).attr('data-position')) ? $(this).attr('data-position') : false;         
+            if (offset) {
+                var toMove = parseInt(offset);
+                $('html,body').stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, options.speed);
+            } else if (position) {
+                var toMove = parseInt(position);
+                $('html,body').stop(true, false).animate({scrollTop: toMove }, options.speed);
+            } else {
+                $('html,body').stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, options.speed);
+            }
+        });
+
+    };
+})(jQuery);
+
+
+/* --------------------------------------------------------- */
+// Back To Top
+/* --------------------------------------------------------- */
+
+jQuery(document).ready(function($){
+  // browser window scroll (in pixels) after which the "back to top" link is shown
+  var offset = 300,
+    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+    offset_opacity = 1200,
+    //duration of the top scrolling animation (in ms)
+    scroll_top_duration = 900,
+    //grab the "back to top" link
+    $back_to_top = $('.cd-top');
+
+  //hide or show the "back to top" link
+  $(window).scroll(function(){
+    ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+    if( $(this).scrollTop() > offset_opacity ) { 
+      $back_to_top.addClass('cd-fade-out');
+    }
+  });
+
+  //smooth scroll to top
+  $back_to_top.on('click', function(event){
+    event.preventDefault();
+    $('body,html').animate({
+      scrollTop: 0 ,
+      }, scroll_top_duration
+    );
+  });
+
+});
